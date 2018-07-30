fe4d0e637feb5f5e2924bb56fb27d043adfe1cf1
PokemonJ <pokemonjjj@outlook.com>
2018-07-30 23:29:43 +0800

    test

---
+++ p4/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@Time       : 2018/7/30 0030 下午 11:28
+@Author     : PokemonJ
+@Email      : 
+@File       : __init__.py.py
+@Software   : PyCharm
+@Description:
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print "p"
+    print "p"
+    print "p"
+    print "p"
+    print "p"
+    pass
